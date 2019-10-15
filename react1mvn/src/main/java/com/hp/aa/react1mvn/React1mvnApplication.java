d3efcc8ee005e841e214b7239f27db9bc07a5e6c
Eric Ross <eric.ross@hp.com>
2019-10-15 12:12:35 -0700

    Maven version of react1

---
+++ react1mvn/src/main/java/com/hp/aa/react1mvn/React1mvnApplication.java
@@ -0,0 +1,13 @@
+package com.hp.aa.react1mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class React1mvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(React1mvnApplication.class, args);
+	}
+
+}
---
+++ react1mvn/src/test/java/com/hp/aa/react1mvn/React1mvnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hp.aa.react1mvn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class React1mvnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
