4c3958554b17da056687f5d87182d3c229a61941
Eric Ross <eric.ross@hp.com>
2019-10-15 11:35:58 -0700

    Fresh install

---
+++ react1/src/main/java/com/hp/aa/react1/React1Application.java
@@ -0,0 +1,13 @@
+package com.hp.aa.react1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class React1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(React1Application.class, args);
+	}
+
+}
