0da9188fd1cf4ff80102fa716067297bb7a91b46
Eric Ross <epr@evross.com>
2019-11-24 11:39:44 -0800

    Added dockerproj

---
+++ dockerproj/src/main/java/com/evross/dockerproj/DockerprojApplication.java
@@ -0,0 +1,13 @@
+package com.evross.dockerproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerprojApplication.class, args);
+	}
+
+}
