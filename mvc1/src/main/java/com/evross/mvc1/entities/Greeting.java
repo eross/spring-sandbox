31a3ad54d9eab29c7d3a8c735aee07a2eff0256c
Eric Ross <epr@evross.com>
2019-12-01 09:02:24 -0800

    Play with REST interface

--- mvc1/src/main/java/com/evross/mvc1/entities/Greeting.java
+++ mvc1/src/main/java/com/evross/mvc1/entities/Greeting.java
@@ -2,11 +2,13 @@ package com.evross.mvc1.entities;
 
 public class Greeting {
     private String message;
+    private int score;
 
     public Greeting(){}
 
     public Greeting(String message){
         this.message = message;
+        this.score = 42;
     }
 
     public String getMessage(){
@@ -15,5 +17,13 @@ public class Greeting {
 
     public void setMessage(String message){
         this.message = message;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(Integer score){
+        this.score = score;
     }
 }
