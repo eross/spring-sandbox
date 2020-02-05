6e73afbf7187deaefed3af62752ccf8662bf1545
Eric Ross <eric.ross@hp.com>
2020-02-05 15:39:04 -0800

    Add Eventing

---
+++ eventing/src/test/java/com/hp/sandbox/eventing/EventingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.sandbox.eventing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
